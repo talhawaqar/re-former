66839abdc3c7ebcb06ed7d0b591dda68d3f829e9
umairahmad125 <umairahmad125@gmail.com>
2020-08-18 16:14:50 +0500

    add update method and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  def new; end
+  def new 
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
@@ -8,6 +10,21 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
